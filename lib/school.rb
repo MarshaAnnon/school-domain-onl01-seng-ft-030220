bf3f669f404d7bd8e8a362b883e7daa2d86012ee
Marsha Annon <marshaannon@gmail.com>
2020-03-11 13:49:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,12 +2,14 @@ require "pry"
 
 class School
 
+  attr_reader :roster
+
   def initialize(school)
     @school = school
     @roster = {}
   end
 
-  
+
 end
 
 school = School.new("Bayside High School")
