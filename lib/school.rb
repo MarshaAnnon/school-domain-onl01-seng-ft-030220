fabea039e1445627ddbd4ce1caa76bf20f42199e
Marsha Annon <marshaannon@gmail.com>
2020-03-11 12:55:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,18 +2,14 @@ require "pry"
 
 class School
 
+  attr_accessor :roster
+
+
   def initialize(school)
     @school = school
     @roster = {}
   end
 binding.pry
-  def roster=(roster)
-    @roster = roster
-  end
-
-  def roster
-    @roster = roster
-  end
 
 end
 
